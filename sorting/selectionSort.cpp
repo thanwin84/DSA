4f7a7e30e89f1331621a43a0dd7819f5ab941907
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2024-10-29 19:56:23 +0600

    Update selectionSort.cpp

--- sorting/selectionSort.cpp
+++ sorting/selectionSort.cpp
@@ -1,1 +1,48 @@
 
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <unordered_map>
+#include<set>
+#include <unordered_set>
+
+using namespace std;
+void printArray(vector<int>& arr) {
+    for (auto i : arr) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+class Solve {
+public:
+    vector<int> selectionSort(vector<int>& arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            const int temp = arr[i];
+            int _min = temp;
+            int minIndex = i;
+            for (int j = i; j < arr.size(); j++) {
+                if (arr[j] < _min) {
+                    _min = arr[j];
+                    minIndex = j;
+                }
+            }
+            arr[i] = _min;
+            arr[minIndex] = temp;
+        }
+        return arr;
+    }
+};
+
+int main()
+{
+    vector<int> arr = { 20, 30, 4, 2, 8 };
+    Solve obj;
+    vector<int> output = obj.selectionSort(arr);
+    printArray(output);
+    return 0;
+}
+
+// time complexity: O(n^2)
