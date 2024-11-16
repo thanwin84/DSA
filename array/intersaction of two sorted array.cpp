917817c3cdce718043a6ff3bab1b8588dd5a75b6
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2024-11-16 20:49:49 +0600

    Update intersaction of two sorted array.cpp

--- array/intersaction of two sorted array.cpp
+++ array/intersaction of two sorted array.cpp
@@ -8,9 +8,11 @@ class Solution{
           for (int i = 0; i < n1; i++) {
               for (int j = 0; j < n2; j++) {
                   if (arr1[i] == arr2[j] && visited[j] == 0) {
-                      ans.push_back(arr1[i]);
-                      visited[j] = 1;
-                      break;
+                      if (ans.empty() || ans.back() != arr1[i]){
+                          ans.push_back(arr1[i]);
+                          visited[j] = 1;
+                          break;
+                      }
                   }
               }
           }
