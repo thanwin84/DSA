b455f13c30dc0888c2bcf633bf7a85ad543beb47
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2024-11-25 17:19:18 +0600

    Update maximum subarray sum.cpp

--- array/maximum subarray sum.cpp
+++ array/maximum subarray sum.cpp
@@ -25,3 +25,35 @@ public:
 };
 
 // tc: O(n) and space: O(1)
+
+// what if we want to print the sub array
+class Solution {
+public:
+    void maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int currentSum = 0;
+        int left = 0;
+        int right = 0;
+        int tempLeft = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            currentSum += nums[i];
+            // if current sum is larger than maxSum, update maxSum
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+                right = i;
+                left = tempLeft;
+            }
+            // if currentSum becomes less than 0, we can discard the current subarray and start new
+            // negetive will never contribute to larger sum
+            if (currentSum < 0) {
+                currentSum = 0;
+                tempLeft = i + 1; // start new sub array from next index
+            }
+
+        }
+        for (int i = left; i <= right; i++) {
+            cout << nums[i] << " ";
+        }
+        cout << endl;
+    }
+};
